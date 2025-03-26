a30fd8a1481917baff5ecbdca26f4e384904e0a0
micheal <micheal@colhountech.com>
2025-03-26 08:14:56 +0000

    AuthSample.EntraID initial with None

--- AuthSample.AppHost/Program.cs
+++ AuthSample.AppHost/Program.cs
@@ -4,4 +4,6 @@ builder.AddProject<Projects.AuthSample_None>("authsample-none");
 
 builder.AddProject<Projects.AuthSample_Individual>("authsample-individual");
 
+builder.AddProject<Projects.AuthSample_EntraID>("authsample-entraid");
+
 builder.Build().Run();
