055879c8ffdf25b413a9d6fcbebadfc294752b16
micheal <micheal@colhountech.com>
2025-03-26 07:59:03 +0000

    Individual Starting with None

--- AuthSample.AppHost/Program.cs
+++ AuthSample.AppHost/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.AuthSample_None>("authsample-none");
 
+builder.AddProject<Projects.AuthSample_Individual>("authsample-individual");
+
 builder.Build().Run();
