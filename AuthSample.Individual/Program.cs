d820a50282282c5c1f414e589687800be0ec2d48
micheal <micheal@colhountech.com>
2025-03-26 08:13:55 +0000

    AuthSample Individual Accounts (ASP.NET Core Identity) Diff

--- AuthSample.Individual/Program.cs
+++ AuthSample.Individual/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using AuthSample.Individual.Data;
+
 namespace AuthSample.Individual;
 
 public class Program
@@ -8,6 +12,13 @@ public class Program
         builder.AddServiceDefaults();
 
         // Add services to the container.
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
+        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddEntityFrameworkStores<ApplicationDbContext>();
         builder.Services.AddRazorPages();
 
         var app = builder.Build();
@@ -15,7 +26,11 @@ public class Program
         app.MapDefaultEndpoints();
 
         // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseMigrationsEndPoint();
+        }
+        else
         {
             app.UseExceptionHandler("/Error");
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
---
+++ AuthSample.Individual/README.md
@@ -0,0 +1,5 @@
+Install-Package Microsoft.EntityFrameworkCore.Tools
+
+Update-Database -Context AuthSample.Individual.Data.ApplicationDbContext -Project AuthSample.Individual  -StartupProject AuthSample.Individual 
+
+
