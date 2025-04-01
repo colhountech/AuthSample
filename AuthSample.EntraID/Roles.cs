b3a60ffb037e548e983e46209f432da6e53a015a
micheal <micheal@colhountech.com>
2025-04-01 14:49:16 +0100

    Update Roles

--- AuthSample.EntraID/Roles.cs
+++ AuthSample.EntraID/Roles.cs
@@ -1,5 +1,5 @@
 ﻿
 public static class Roles
 {
-    public const string HQUsers = "396f4717-c69b-41d6-a082-695426638fa6"; // ObjectID, not SID
+    public const string HQUsers = "396f4717-c69b-41d6-a082-6954266300000"; // ObjectID, not SID
 }
